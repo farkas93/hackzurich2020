427417c3b0c65c71294a0490504aaa2a87d70882
kalotayz <zsombor.kalotay@hest.ethz.ch>
2020-09-19 17:20:26 +0200

    Write the product information getter script

---
+++ Assets/Resources/Scripts/ProductInformationGetter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductInformationGetter : MonoBehaviour
+{
+
+    public Transform infoPrefab;
+
+    private Transform productInformation;
+
+    public void getProductInformation()
+    {
+        string productID = "";
+
+            
+        if (ConfigsManager.MappingConfig.TryGetValue(gameObject.name, out productID))
+        {
+            Debug.Log("For key = \""+ gameObject.name + "\", productID =" +  productID);
+            Product p = API.GetProduct(productID);
+
+
+            //TODO: set texts in game objects accordingly
+            Instantiate(infoPrefab, this.transform);
+        }
+        else
+        {
+            Debug.LogWarning("Key = \"" + gameObject.name + "\" is not found.");
+        }
+    }
+
+    public void destroyProductInformation()
+    {
+        Destroy(productInformation);
+    }
+
+}
