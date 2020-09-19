f8d0c92e74d7247707733ad25eafa674aebf4246
kalotayz <zsombor.kalotay@hest.ethz.ch>
2020-09-19 19:12:40 +0200

    hack ooon costi! (Created Access Script for the text game objects)

---
+++ Assets/AccessInfoPrefabTexts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccessInfoPrefabTexts : MonoBehaviour
+{
+    public Text ingredientsText;
+    public Text productNameText;
+    public Text productPriceText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
