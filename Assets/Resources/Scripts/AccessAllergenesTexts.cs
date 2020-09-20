7dd5dae19cfb0c63d29469678016532114fcb346
kalotayz <zsombor.kalotay@hest.ethz.ch>
2020-09-20 04:23:14 +0200

    iclude all icons for allergenes and fix scrollbar

--- Assets/Resources/Scripts/AccessAllergenesTexts.cs
+++ Assets/Resources/Scripts/AccessAllergenesTexts.cs
@@ -9,10 +9,26 @@ public class AccessAllergenesTexts : MonoBehaviour
     List<GameObject> allergenList;
     public Sprite[] allergeneIcons;
 
+    private static Dictionary<string, Sprite> _mappingPicture;
+
 
     public void Awake()
     {
         allergenList = new List<GameObject>();
+
+        _mappingPicture = new Dictionary<string, Sprite>(){
+            {"ALLG_GLUTEN", allergeneIcons[0]},
+            {"ALLG_MILCH", allergeneIcons[1]},
+            {"ALLG_EIER", allergeneIcons[2]},
+            {"ALLG_SOJA", allergeneIcons[3]},
+            {"ALLG_NUSS", allergeneIcons[4]},
+            {"ALLG_MANDELN", allergeneIcons[5]},
+            {"ALLG_HASELNU", allergeneIcons[6]},
+            {"ALLG_BAUMNUS", allergeneIcons[7]},
+            {"ALLG_MACADAM", allergeneIcons[8]},
+            {"ALLG_PISTAZI", allergeneIcons[9]},
+            {"ALLG_SESAM", allergeneIcons[10]}
+        };
     }
 
     // Update is called once per frame
@@ -37,8 +53,11 @@ public class AccessAllergenesTexts : MonoBehaviour
         foreach(Allergen a in allergens)
         {
             Transform g = Instantiate(allergenePrefab, content);
-            Sprite s = null;
-            g.GetComponent<AccessAllergenEntries>().UpdateInfo(a, s);
+            Sprite s;
+            if (_mappingPicture.TryGetValue(a.code, out s))
+                g.GetComponent<AccessAllergenEntries>().UpdateInfo(a, s);
+            else
+                g.GetComponent<AccessAllergenEntries>().UpdateInfo(a, null);
             allergenList.Add(g.gameObject);
         }
     }
